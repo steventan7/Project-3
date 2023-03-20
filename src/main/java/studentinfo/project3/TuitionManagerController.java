302a562766b1307aaf6534978ee95a849e8113e5
nolipot <david.f2002@outlook.com>
2023-03-20 16:06:27 -0400

    credit checking for roster

--- src/main/java/studentinfo/project3/TuitionManagerController.java
+++ src/main/java/studentinfo/project3/TuitionManagerController.java
@@ -49,8 +49,6 @@ public class TuitionManagerController {
     public static final int SIXTEENYEARS = 16;
     public static final int FULLTIMECREDITMIN = 12;
     public static final int MAXAMOUNT = 10000;
-    public static final int MAXCREDITS = 24;
-    public static final int MINCREDITS = 3;
     private static Roster studentRoster = new Roster();
     private static Enrollment studentEnrollment = new Enrollment();
 
@@ -288,14 +286,6 @@ public class TuitionManagerController {
         if(isNumeric(input)) {
             int cred = Integer.parseInt(input);
             if(cred >= 0) {
-                if(cred < MINCREDITS) {
-                    output.setText("Credits completed invalid: cannot be less than 3.");
-                    return -1;
-                }
-                if(cred > MAXCREDITS) {
-                    output.setText("Credits completed invalid: cannot be more than 24.");
-                    return -1;
-                }
                 return cred;
             }
             output.setText("Credits completed invalid: cannot be negative!");
